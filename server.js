ec927bbaa4d8e4c7926845bdde96a828ffa5ab01
Stephane <kalonjistephane@gmail.com>
2021-04-08 15:47:00 +0200

    Initalize socket on server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+// Modules import
 const path = require('path')
-
+const http = require('http')
 const express = require('express');
+const socketio = require('socket.io')
 
+// Initalize socket on server
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(3000, ()=>{
+server.listen(3000, ()=>{
     console.log(`Server is running on ${PORT}`)
 });
