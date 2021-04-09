daccae509008bde7cd08bf7e976104b86bb4ab19
Stephane <kalonjistephane@gmail.com>
2021-04-09 12:09:05 +0200

    emit msg client-server-display in client's screen

--- public/js/main.js
+++ public/js/main.js
@@ -2,17 +2,29 @@ const chatForm = document.getElementById('chat-form');
 
 const socket = io(); // we can use it as we import socket.io in the script (chat.html-line 60)
 
+// Message from server
 socket.on('message',message=>{
-    console.log(message)
-})
+    console.log(message);
+    outputMessage(message);
+
+});
 
 // Message submit
 chatForm.addEventListener('submit', (e)=>{
     e.preventDefault();
 
     //Get message text
-    const msg = document.getElementById('msg').value;
+    const msg = e.target.elements.msg.value; // = document.getElementById('msg').value
     
     //Emit message to server
     socket.emit('chatMessage', msg)
-})
+});
+
+// Output message to DOM
+const outputMessage = (message)=>{
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Steph <span>9:15pm</span></p>
+                     <p class="text"> ${message} </p>`;
+    document.querySelector('.chat-messages').appendChild(div)
+}   
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection',socket =>{
 
     //Listen for chatMessage
     socket.on('chatMessage', (msg)=>{
-        console.log(msg)
+        io.emit('message',msg)
     })
 })
 
